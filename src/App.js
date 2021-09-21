b71099aa8c6613f7f523f5d14a436dab9845d178
DreamWillBeReal <120799088+DarkHorseCorder@users.noreply.github.com>
2021-09-22 01:53:33 +0900

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './pages/landingpage.js'
+import React, {useEffect, useState} from 'react';
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
